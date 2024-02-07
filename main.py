cd5c32011c642d718e3c1d44fd9e1e9e614375da
SamuelPerezCO <sernasamuelperez@gmail.com>
2024-02-07 14:43:44 -0500

    Prime linea de codigo

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Hello world")
+from tkinter import *
+from ventana import *
+
+def main():
+    root = Tk()
+    root.wm_title("Crud Pyrhon MySql")
+    app = Ventana(root)
+    app.mainloop()
+
+if __name__ == "__main__":
+    main() 
