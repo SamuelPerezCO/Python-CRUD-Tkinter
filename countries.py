03d5a2e7d8171b53a8a863c3e45735043c4ed8d3
SamuelPerezCO <sernasamuelperez@gmail.com>
2024-02-09 18:08:07 -0500

    Clase countries culminada

--- countries.py
+++ countries.py
@@ -1,26 +1,34 @@
 import mysql.connector
 
-class Cities:
+class Countries:
 
     def __init__(self):
         self.cnn = mysql.connector.connect(host="localhost" , user="root",
         passwd = "1234" , database="bdejemplopy")
 
     def __str__(self):
-        datos = self.consulta_ciudades()
+        datos = self.consulta_paises()
         aux = ""
         for row in datos:
             aux = aux + str(row) + "\n"
         return aux
 
-    def consulta_ciudades(self):
+    def consulta_paises(self):
         cur = self.cnn.cursor()
         cur.execute("SELECT * FROM countries")
         datos = cur.fetchall()
         cur.close()
         return datos
     
-    def inserta_ciudad(self ,ISO3 , CountryName , Capital , CurrencyCode):
+    def buscar_pais(self , Id):
+        cur = self.cnn.cursor()
+        sql = f"SELECT * FROM countries WHERE Id = '{Id}'"
+        cur.execute(sql)
+        datos = cur.fetchone()
+        cur.close()
+        return datos
+    
+    def inserta_pais(self ,ISO3 , CountryName , Capital , CurrencyCode):
         cur = self.cnn.cursor()
         sql = f'''INSERT INTO Countries(ISO3 , CountryName , Capital , CurrencyCode)
         VALUES('{ISO3}' , '{CountryName}' , '{Capital}', '{CurrencyCode}')'''
@@ -30,7 +38,7 @@ class Cities:
         cur.close()
         return n
 
-    def elimina_ciudad(self, Id):
+    def elimina_pais(self, Id):
         cur = self.cnn.cursor()
         try:
             sql = f'''DELETE FROM countries WHERE Id = '{Id}' '''
@@ -43,7 +51,7 @@ class Cities:
             print("Error al eliminar el registro:", e)
             return -1
     
-    def modifica_cuidad(self , Id , ISO3 , CountryName , Capital , CurrencyCode):
+    def modifica_pais(self , Id , ISO3 , CountryName , Capital , CurrencyCode):
         cur = self.cnn.cursor()
         sql = f'''UPDATE countries set ISO3='{ISO3}' , CountryName = '{CountryName}' , Capital = '{Capital}' ,
         CurrencyCode = '{CurrencyCode}' WHERE Id = '{Id}'
--- main.py
+++ main.py
@@ -1,6 +1,71 @@
-from Cities import Cities 
+from Cities import Countries
 
-ciudades = Cities()
+paises = Countries()
+
+menu = '''==========================
+* MENU paises *
+==========================
+*                        *
+* i) Insertar Registros  *
+* e) Eliminar Registros  *
+* m) Modificar Registros *
+* p) Imprimir la tabla   *
+* x) Salir               *
+*                        *
+=========================='''
+
+def main():
+    opcion = '0'
+    while opcion != 'X':
+        print(menu)
+        opcion = input("¿Que opcion deseas? : ").upper()
+        if opcion == 'I':
+            print("Insertar paises")
+            ISO3 = input("Introduce la clave ISO3 del Nuevo Pais: ")
+            CountryName = input("Introduce el nombre del nuevo pais: ")
+            Capital = input("Introduce la Capital de nuevo Pais: ")
+            CurrencyCode = input("Introduce el currency code del nuevo pais: ")
+            r = paises.inserta_pais(ISO3 , CountryName ,Capital, CurrencyCode )
+            if(r == 0):
+                print("-> Error al insertar")
+            else:
+                print("-> Se inserto correctamente")
+        elif opcion == 'E':
+            print("Eliminar paises")
+            Id = int(input("Introduce el Id del pais que desea Eliminar: "))
+            r = paises.elimina_pais(Id)
+            if(r == 0):
+                print("El Pais no existe")
+            else:
+                print("-> el pais se elimino Correctamente")
+        elif opcion == 'M':
+            print("Modificar paises")
+            Id = int(input("Introduce el Id del pais que quieres modificar: "))
+            pais = paises.buscar_pais(Id)
+            if pais == None:
+                print("-> EL pais no existe")
+            else:
+                print("Pais a modificar: ")
+                print(pais)
+                ISO3 = input("Introduce la nueva clave ISO3 del pais a modificar: ")
+                CountryName = input("Introduce el nuevo nombre del pais a modificar: ")
+                Capital = input("Introduce la Capital del pais a modificar: ")
+                CurrencyCode = input("Introduce el currency code del pais a modificar: ")
+                r = paises.modifica_pais(Id , ISO3 , CountryName , Capital , CurrencyCode)
+                if r == 0:
+                    print("Modificacion no completada")
+                else:
+                    print("Se Modifico Correctamente")
+        elif opcion == 'P':
+            print("Imprimir paises")
+            print(paises)
+        elif opcion == 'X':
+            print("-> Saliendo del sistema")
+        else:
+            print("Opcion no valida")
+
+    pass
+
 '''
 from tkinter import *
 from ventana import *
@@ -11,7 +76,6 @@ def main():
     root.wm_title("Crud Python MySql")
     app = Ventana(root)
     app.mainloop()
-
+'''
 if __name__ == "__main__":
     main() 
-'''
